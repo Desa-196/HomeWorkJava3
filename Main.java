8f239eca9a82fa5e376651dd26f70297d7f69168
Денис Пономарев <deskin196@gmail.com>
2023-07-03 16:22:55 +0300

    Написал функцию возвращающую новый массив с удаленными четными числами

--- Main.java
+++ Main.java
@@ -10,8 +10,31 @@
 
  */
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main{
-    public static void main() {
-        
+    public static void main(String[] args) {
+        ArrayList<Integer> list = generateRandomArrayList(20, 10, 0);
+        System.out.println(list);
+        System.out.println(deleteEvenArrayList(list));
+    }
+    public static ArrayList<Integer> generateRandomArrayList(int size, int max, int min) {
+        ArrayList<Integer> list = new ArrayList<Integer>(size);
+        for(int i=0; i < size; i++){
+            list.add((int)(Math.random()*(max - min + 1) + min));
+        }
+        return list;
+    }
+    public static ArrayList<Integer> deleteEvenArrayList(ArrayList<Integer> list) {
+        ArrayList<Integer> resultList = new ArrayList<Integer>();
+        Iterator<Integer> iterator = list.iterator();
+        while(iterator.hasNext()){
+            Integer o = iterator.next();
+            if(o % 2 != 0){
+                //resultList.add(o);
+            }
+        }
+        return resultList;
     }
 }
